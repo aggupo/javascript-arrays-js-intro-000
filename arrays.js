1d43bb9f0cb330775243190024a2bb396ce05eec
Li Ge <gelber.libi@gmail.com>
2018-04-08 06:53:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
   var newarray = [array];
-  return newarray.unshift(element);
+  return [newarray].unshift(element);
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
@@ -36,5 +36,3 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 // 7
-
-
